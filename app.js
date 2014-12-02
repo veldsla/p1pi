136141238d5e44ca9b3487d6500f8ea59db40161
Arno Velds <avelds@gmail.com>
2014-12-02 21:10:32 +0100

    Some epxress 4 edits

--- app.js
+++ app.js
@@ -10,34 +10,34 @@ var express = require('express')
 
 var p1 = require('./p1data');
 var dbi = require('./dbi');
+var dev = require('./routes/dev')
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+ app.set('port', process.env.PORT || 3000);
+ app.set('views', __dirname + '/views');
+ app.set('view engine', 'jade');
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+ app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', routes.index);
+app.get('/dev', dev.index);
 app.get('/livedatapoint', function(req, res) {
 	res.json(p1.getLastMeasurement());
 });
 app.get('/livehistory', function(req, res) {
 	res.json(p1.getMeasurements());
 });
+
 app.get('/gashistory', function(req, res) {
 	dbi.getLastweekGas(function(err, rows) {
+		res.json(rows);
+	});
+});
+
+app.get('/gasweekdag', function(req, res) {
+	dbi.getGasPerDag(function(err, rows) {
 		res.json(rows);
 	});
 });
